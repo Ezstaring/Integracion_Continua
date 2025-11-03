105634efc85dd033affc2fca6eaa4a051d5e40fa
Ezstaring <ezstaring@gmail.com>
2025-11-03 16:02:22 -0500

    modified views.py

--- api_productos/backend/productos/views.py
+++ api_productos/backend/productos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Producto
+from .serializer import ProductoSerializer
 # Create your views here.
+
+class ProductoViewSet(viewsets.ModelViewSet):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+    
