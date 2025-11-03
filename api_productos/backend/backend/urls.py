c18ec0d3b713a6f3b527ea20f4dbc62e66196769
Ezstaring <ezstaring@gmail.com>
2025-11-03 16:54:16 -0500

    more configurations but this is for the tokenization so is so easy

--- api_productos/backend/backend/urls.py
+++ api_productos/backend/backend/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('productos.urls')),
     path('', views.home, name='home'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
